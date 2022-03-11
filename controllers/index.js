248e7093637deccdacfdd2c1cfd39e380ab322e2
Mike Gordon <vprmatrix55@gmail.com>
2022-03-10 18:41:14 -0700

    cleaned up file structure

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
-const router = require('express').Router();
+// const router = require('express').Router();
 
-const homeRoutes = require('./home-routes.js');
-const apiRoutes = require('./api');
-const dashboardRoutes = require('./dashboard-routes.js');
+// const homeRoutes = require('./home-routes.js');
+// const apiRoutes = require('./api');
+// const dashboardRoutes = require('./dashboard-routes.js');
 
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
-router.use('/dashboard', dashboardRoutes);
+// router.use('/', homeRoutes);
+// router.use('/api', apiRoutes);
+// router.use('/dashboard', dashboardRoutes);
 
-module.exports = router;
+// module.exports = router;
